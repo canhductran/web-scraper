142e4b650fe08c9806c2d6bb0f3ba7ad674e60a1
Chris Tran <chris.canhductran@gmail.com>
2022-10-27 21:35:28 +1100

    added yarn 3 + typescript

---
+++ src/puppeteer/index.ts
@@ -0,0 +1,15 @@
+import * as puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 100,
+    devtools: true,
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  await page.pdf({path: 'google.pdf'});
+
+  await browser.close();
+})();
