d94a9e6d6f06e068e21d1cc48389f8eb172c071b
Chris Tran <chris.canhductran@gmail.com>
2022-10-31 00:22:29 +1100

    add more scraping logic

--- src/puppeteer/index.ts
+++ src/puppeteer/index.ts
@@ -8,12 +8,39 @@ import * as puppeteer from "puppeteer";
   });
 
   const page = await browser.newPage();
-  await page.goto('https://vanbanphapluat.co/csdl/van-ban-phap-luat');
-  await page.waitForSelector('.page_inner');
+  await page.goto("https://vanbanphapluat.co/csdl/van-ban-phap-luat");
+  await page.waitForSelector(".block-content");
 
-  const urls = await page.$$eval('div.block-content', links => {
-    console.log('links', links);
+  const processEachPage = async (url) => {
+    console.log('url', url);
+    const newPage = await browser.newPage();
+    await newPage.goto(url);
+    await newPage.waitForSelector(".block-content");
+
+    const [numberElement] = await newPage.$x("//td[text() = 'Số hiệu']/following-sibling::td[1]");
+    const number = await newPage.evaluate(element => element.textContent, numberElement);
+
+    console.log('number', number);
+  };
+  const urls = await page.$$eval("div.block-content", (links) => {
+    console.log("links", links);
+    const newLinks = links.map(
+      (link) => (link.querySelector("a.col-md-2") as any)?.href
+    );
+
+    console.log("newLinks", newLinks);
+    return newLinks.filter((l) => !!l);
   });
 
-  await browser.close();
+  for (const url of urls) {
+    await processEachPage(url);
+    // console.log('url', url);
+  }
+  console.log("urls", urls);
+
+  // const dataObject = {};
+  // const newPage = await browser.newPage();
+  // await newPage.goto(link);
+
+  // await browser.close();
 })();
